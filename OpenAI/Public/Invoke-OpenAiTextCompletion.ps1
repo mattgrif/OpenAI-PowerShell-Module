5ea8bbf7ce5c0a4adfe94d80dc1f1ee1a966ea17
Matt Griffin <Matt@MattBlogsIt.com>
2023-01-16 19:18:26 -0500

    Working Save and Invoke command for Completions.

--- OpenAI/Public/Invoke-OpenAiTextCompletion.ps1
+++ OpenAI/Public/Invoke-OpenAiTextCompletion.ps1
@@ -2,33 +2,61 @@ function Invoke-OpenAiTextCompletion {
     [CmdletBinding()]
     param (
 		[parameter(Mandatory=$true)]
-        $Token,
-		[parameter(Mandatory=$true)]
-        $Model,
-		[parameter(Mandatory=$true)]
         $Prompt,
-		$Suffix,
-        $MaxTokens,
-        [ValidateRange(0, 1)]
-        $Temperature = 0.75,
-		$TopP,
-		$N,
-		$Stream,
-		$LogProbs,
-		$Echo,
-		$Stop
+        $Model,
+		# $Suffix, These Parameters have yet to be implemented.
+        $MaxTokens
+        # [ValidateRange(0, 1)]
+        # $Temperature = 0.75,
+		# $TopP,
+		# $N,
+		# $Stream,
+		# $LogProbs,
+		# $Echo,
+		# $Stop
     )
 
     begin {
-        $headers = @{
-            Authorization = "Bearer $token"
+        #Read API Key
+        $secureToken = [Environment]::GetEnvironmentVariable('OpenAiToken', 'User')
+
+        if(![string]::IsNullOrEmpty($secureToken)) {
+            $cred = New-Object pscredential 'fakeuser', (ConvertTo-SecureString $secureToken)
+            $plainToken = $cred.Password | ConvertFrom-SecureString -AsPlainText
+        } else {
+            Write-Output "You must save your OpenAI Connection String before continuing."
+            Save-OpenAiConnection
+
+            #Now read newly saved token
+            $cred = New-Object pscredential 'fakeuser', (ConvertTo-SecureString [Environment]::GetEnvironmentVariable('OpenAiToken', 'User'))
+            $plainToken = $cred.Password | ConvertFrom-SecureString -AsPlainText
         }
 
+        #Check for Model Param, read global setting if not set
+        if([string]::IsNullOrEmpty($Model)) {
+            $Model = [Environment]::GetEnvironmentVariable('OpenAiModel', 'User')
+        }
+
+        #Build the Header API Call
+        $headers = @{
+            Authorization = "Bearer $plainToken"
+        }
+
+        #Build base body for required Parameters
         $body = @{
             model = $Model
             prompt = $Prompt
-            temperature = $Temperature
+            #temperature = $Temperature
         }
+
+        #Add Optional Parameters to the body
+        if(![string]::IsNullOrEmpty($MaxTokens)){
+            #Max_Tokens Optional Parameter
+            $body += @{
+                max_tokens = $MaxTokens
+            }
+        }
+
     }
 
     process {
